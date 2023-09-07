f4f102bb0834b4dad173ec791b5e6d982a9bbdc5
Zacharay <99123665+Zacharay@users.noreply.github.com>
2023-09-07 14:42:57 +0200

    Add files via upload

--- php/views/loginForm.php
+++ php/views/loginForm.php
@@ -1,7 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../styles/main.css">
+    <link rel="stylesheet" href="../../styles/loginForms.css">
+    <title>Document</title>
+</head>
 <body>
-    <div class="loginBox-container">
+<div class="loginBox-container">
         <h1 class="loginBox-title">SIGN IN</h1>
         <form class="login-form">
             <input type="text" name="Username" class="loginBox-textInput" placeholder="Username">
