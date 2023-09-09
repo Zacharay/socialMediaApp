5790d30c39e19aefcf83fc51b6dcd6de387090ee
Zachariasz Słupski <zachary.slupski@wp.pl>
2023-09-09 16:00:33 +0200

    design for upload photo form implemented

--- php/views/uploadPhoto.php
+++ php/views/uploadPhoto.php
@@ -1,5 +1,21 @@
-<?php
-
-echo "zalogowales sie poprawnie";
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../styles/main.css">
+    <link rel="stylesheet" href="../../styles/loginForm.css">
+    <title>Upload Photo</title>
+</head>
+<body>
+   
+    <div class="loginBox-container uploadPhoto-container">
+        <img  src="../../images/profilePhotos/defaultPhoto.png" class="profile-photo"/>
+        <form id="uploadPhotoForm" method="POST">
+            <input type="file" name="profilePhoto" id="profilePhotoInput"/>
+            <label for="profilePhotoInput" class="profilePhotoInput-label">Choose an image </label>
+            <input type="submit" value="Save" class="loginBox-submit"/>
+        </form>
+    </div>
+</body>
+</html>
