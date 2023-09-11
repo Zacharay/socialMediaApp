1a934c167ef5b399a97470201de3c4c474b1b6da
Zachariasz Słupski <zachary.slupski@wp.pl>
2023-09-11 23:30:38 +0200

    post design implemented

--- php/views/userProfile.php
+++ php/views/userProfile.php
@@ -8,8 +8,8 @@
     <title>Social Media</title>
 </head>
 <body>
-    <div style="background-color: #f5f7fe; box-shadow: 0px 13px 24px -22px rgba(66, 68, 90, 1);">
-        <section class="userProfile__header container">
+    <header class="userProfile__section--gray">
+        <div class="userProfile__header container">
             <div class="userProfile__header__wrapper">
                 <img src="../../images/profilePhotos/userPhoto_1.png" class="userProfile__photo"/>
                 <div>
@@ -18,6 +18,7 @@
                     <p class="userProfile__description">
                         <span>Professional Experience</span></br>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste totam deserunt qui, suscipit rem minima, amet pariatur ab aperiam explicabo harum, voluptate maxime. Nulla nesciunt assumenda eaque facere laborum eveniet minus. Aut maiores placeat soluta possimus. Provident nulla aut et.
+                        lorem40
                     </p>
                 </div>
             </div>
@@ -45,10 +46,9 @@
                     </li>
                 </ul>
                 <button class="btn--primary">Send Message</button>
-                
             </div>
-        </section>
-        <section class="followers__container container">
+        </div>
+        <div class="followers__container container">
             <div class="followers__element">
                 <h4>441</h4>
                 <p>Followers</p>
@@ -58,7 +58,27 @@
                 <p>Following</p>
             </div>
             <button class="btn--primary followers__btn">Follow</button>
-        </section>
-    
+        </div>
+    </header>
+    <section class="container post__section">
+        <div class="post__container">
+            <div class="post__info">
+                <img src="../../images/profilePhotos/userPhoto_62.png" alt="profile photo" class="post__userphoto"/>
+                <div>
+                    <h2 class="post__username">Bill Gates</h2>
+                    <h3 class="post__date">Created at: 11-09-2023</h3>
+                </div>
+            </div>
+            <div class="post__content">
+                <p class="post__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos molestias iste est. Eius cupiditate reiciendis dolore vero, esse necessitatibus similique voluptate laboriosam. Amet delectus similique illum dicta expedita autem quo dolorem, optio sunt cupiditate deserunt totam eum fuga sit ratione aperiam iusto omnis maxime! Dolore nostrum voluptatum doloribus numquam fugit laborum, et impedit corrupti? Ab, temporibus. Quod consequatur facilis vel. Alias quae animi quisquam delectus praesentium id dignissimos. Vel eos animi laborum dicta distinctio rerum adipisci illum et doloremque id obcaecati, sed odio ad voluptatum optio? Temporibus pariatur earum facere quisquam aut, exercitationem laborum cumque fuga vitae qui at corporis.</p>
+                <img class="post__image" src="../../images/postPhotos/postPhoto--1.webp" alt="post image"/>
+            </div>
+            <div class="post__action__container">
+                <div>Like </div>
+                <div>Comment</div>
+                <div>Share</div>
+            </div>
+        </div>
+    </section>
 </body>
 </html>
