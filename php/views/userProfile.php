5fb4e2e1ee27e3762edd538995d8d0f624a91451
Zacharay <99123665+Zacharay@users.noreply.github.com>
2023-09-11 14:44:09 +0200

    Add files via upload

--- php/views/userProfile.php
+++ php/views/userProfile.php
@@ -8,21 +8,46 @@
     <title>Social Media</title>
 </head>
 <body>
-    <section class="userProfile__header">
-
-        <img src="../../images/profilePhotos/userPhoto_62.png" class="userProfile__photo"/>
-        <div>
-            <h1 class="userProfile__name">Bill Gates</h1>
-            <h2 class="userProfile__occupation">CEO AT MICROSOFT</h2>
-        </div>
-        <div>
-            <li>
-                <ul>fb</ul>
-                <ul>linkedin</ul>
-                <ul>instagram</ul>
-            </li>
-            <button>Send Message</button>
-        </div>
-    </section>
+    <div style="background-color: #f5f7fe;">
+        <section class="userProfile__header container">
+            <div class="userProfile__header__wrapper">
+                <img src="../../images/profilePhotos/userPhoto_1.png" class="userProfile__photo"/>
+                <div>
+                    <h1 class="userProfile__name">Bill Gates</h1>
+                    <h2 class="userProfile__occupation">CEO AT MICROSOFT</h2>
+                    <p class="userProfile__description">
+                        <span>Professional Experience</span></br>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste totam deserunt qui, suscipit rem minima, amet pariatur ab aperiam explicabo harum, voluptate maxime. Nulla nesciunt assumenda eaque facere laborum eveniet minus. Aut maiores placeat soluta possimus. Provident nulla aut et.
+                    </p>
+                </div>
+            </div>
+            <div>
+                <ul class="userProfile__socials__container">
+                    <li>
+                        <a href="https://facebook.com" target="_blank">
+                            <img src="../../images/icons/facebookIcon.webp" alt="Facebook Logo" class="userProfile__socials__image"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com" target="_blank">
+                            <img src="../../images/icons/twitterIcon.png" alt="Twitter Logo" class="userProfile__socials__image"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://instagram.com" target="_blank">
+                            <img src="../../images/icons/instagramIcon.webp" alt="Instagram Logo" class="userProfile__socials__image"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://linkedin.com" target="_blank">
+                            <img src="../../images/icons/linkedinIcon.webp" alt="Linkedin Logo" class="userProfile__socials__image"/>
+                        </a>
+                    </li>
+                </ul>
+                <button>Send Message</button>
+            </div>
+        </section>
+    </div>
+    
 </body>
 </html>
