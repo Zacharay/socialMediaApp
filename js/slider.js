03e0d4eecb298e1f9c503111b87f57d14cac9cce
Zachariasz Słupski <zachary.slupski@wp.pl>
2023-09-14 18:07:18 +0200

    slider finished

---
+++ js/slider.js
@@ -0,0 +1,34 @@
+function initializeSlider(container) {
+    let currentSlide = 0;
+    const nextBtn = container.querySelector(".slider__next");
+    const prevBtn = container.querySelector(".slider__prev");
+    const slider = container.querySelector(".slider");
+    const slides = Array.from(slider.children);
+  
+    const nextSlide = function () {
+        currentSlide++;
+        if (currentSlide >= slides.length) currentSlide = 0;
+        updateSlider();
+    };
+  
+    const prevSlide = function () {
+        currentSlide--;
+        if (currentSlide < 0) currentSlide = slides.length - 1;
+        updateSlider();
+    };
+  
+    function updateSlider() {
+        
+      slides.forEach((slide, index) => {
+        console.log("test");
+        slide.style= `transform:translateX(${-currentSlide * 100}%);`;
+      });
+    }
+  
+    nextBtn.addEventListener("click", nextSlide);
+    prevBtn.addEventListener("click", prevSlide);
+}
+
+const postContainers = document.querySelectorAll(".post__container");
+
+postContainers.forEach(initializeSlider);
